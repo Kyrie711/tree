dc4e8e9ff8fb60804098ecb09e5d8a7a37a29647
IsPinocchio <1149958259@qq.com>
2022-02-13 00:48:55 +0800

    test

---
+++ src/constant/url.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-13 00:48:19
+ * @LastEditTime: 2022-02-13 00:48:25
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \春阳tree\tree\src\constant\url.js
+ */
+
+export const url1 = ''
