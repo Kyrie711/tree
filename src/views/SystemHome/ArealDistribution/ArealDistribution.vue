98b1ee23a8bfaea09f688e97cde5477d6b1ee0bd
kkkkyrie <2323740041@qq.com>
2022-02-14 00:23:40 +0800

    test map

--- src/views/SystemHome/ArealDistribution/ArealDistribution.vue
+++ src/views/SystemHome/ArealDistribution/ArealDistribution.vue
@@ -1,85 +1,172 @@
 <template>
-  <div id="arealdistribution">
-    <div class="area_map"></div>
-  </div>
+    <div>
+      <div id="container"></div>
+      <div class="info" style="min-width: 200px;">
+        <h4>地区</h4>
+        <p>中文名字：<span id="name">--</span></p>
+        <p>SOC：<span id="soc">--</span></p><hr>
+        <p>移动至地图任意陆地区域</p>
+      </div>
+
+ </div>
 </template>
-
 <script>
-import * as echarts from 'echarts';
-// import '../../../utils/bmap.min.js'
-require('echarts/extension/bmap/bmap')
+import AMapLoader from '@amap/amap-jsapi-loader';
+
 export default {
-  mounted() {
-    var areamap = document.getElementsByClassName('area_map')[0];
-    var myChart = echarts.init(areamap);
-    var map = new BMapGL.Map(areamap);
-    var point = new BMapGL.Point(106.404, 35.915);
-    map.centerAndZoom(point, 6); 
-    map.enableScrollWheelZoom(true);  
-    map.setMapStyleV2({     
-        styleId: '9baa23e74ec33261c2ec635d0b5ed12b'
+    name:"Mapview",
+    data(){
+        return{
+            map: null,
+            Fill: 'rgba(20, 120, 230, 0.3)',
+            disCountry: null
+        } 
+    },
+    created(){
+
+    },
+    mounted(){
+        // this.initAMap();
+        AMapLoader.load({
+                key:'7deda6e595fc22846191297879e66f8f',  //设置您的key
+                version:"2.0",
+                plugins:['AMap.ToolBar','AMap.Driving'],
+                AMapUI:{
+                    version:"1.1",
+                    plugins:[],
+
+                },
+                Loca:{
+                    version:"2.0"
+                },
+            }).then((AMap)=>{
+                 this.disCountry = new AMap.DistrictLayer.Country({
+                  zIndex: 10,
+                  styles: {
+                      // 颜色格式: #RRGGBB、rgba()、rgb()、[r, g, b, a]
+                      // 国境线
+                      //'nation-stroke': nationStroke,
+                      // 海岸线
+                      //'coastline-stroke': '',
+                      // 填充
+                      'fill': function (props) {
+                          console.log(props.adcode);
+                          if (props.adcode == 420000) {
+                              updateInfo(props);
+                              return 'rgba(20, 120, 230, 0.3)';
+                          } else {
+                              return 'white'
+                          }
+                      }
+                  }
+              });
+
+                this.map = new AMap.Map("container",{
+                  zooms: [5, 18],
+                  center: [110,35],
+                  showIndoorMap: false,
+                  zoom: 3,
+                  isHotspot: false,
+                  defaultCursor: 'pointer',
+                  touchZoomCenter: 1,
+                  pitch: 0,
+                  layers: [
+                    this.disCountry
+                  ],
+                  viewMode: '3D',
+
+                });
+                
+            }).catch(e=>{
+                console.log(e);
+            }).then(() => {
+              let m = this.map
+               this.map.on('complete',() =>{
+        var layer = new AMap.LabelsLayer({
+            // 开启标注避让，默认为开启，v1.4.15 新增属性
+            collision: false,
+            // 开启标注淡入动画，默认为开启，v1.4.15 新增属性
+            animation: true,
+        });
+        
+        for (var i = 0; i < LabelsData.length; i++) {
+            LabelsData[i].text.style.fontSize = 12
+            LabelsData[i].text.style.strokeWidth = 3
+            var labelsMarker = new AMap.LabelMarker(LabelsData[i]);
+            layer.add(labelsMarker);
+        }
+        console.log(this.map);
+        this.map.add(layer);
+    })
+    let disc = this.disCountry
+    this.map.on('mousemove', function (ev) {
+
+        var px = ev.pixel;
+        // 拾取所在位置的行政区
+
+        var props = disc.getDistrictByContainerPos(px);
+
+        if (props) {
+            var adcode = props.adcode;
+            if(adcode){
+                // 重置行政区样式
+                disc.setStyles({
+                    // 国境线
+                    //nation-stroke': nationStroke,
+                    // 海岸线
+                    //'coastline-stroke': '',
+                    'fill': function (props) {
+                        return props.adcode == adcode ? 'rgba(20, 120, 230, 0.3)' : 'white';
+                    }
+
+                });
+                updateInfo(props);
+
+            }
+            
+
+            
+        }
     });
+            })
+
+            function updateInfo(props) {
+              document.getElementById('name').innerText = props.NAME_CHN;
+              document.getElementById('soc').innerText = props.SOC;
+            } 
+        function updateInfo(props) {
+              document.getElementById('name').innerText = props.NAME_CHN;
+              document.getElementById('soc').innerText = props.SOC;
+        } 
 
 
-    // var option = {
-    //     // 加载 bmap 组件
-    //     bmap: {
-    //         // 百度地图中心经纬度
-    //         center: [120.13066322374, 30.240018034923],
-    //         // 百度地图缩放
-    //         zoom: 14,
-    //         // 是否开启拖拽缩放，可以只设置 'scale' 或者 'move'
-    //         roam: true,
-    //         // 百度地图的自定义样式，见 http://developer.baidu.com/map/jsdevelop-11.htm
-    //         mapStyle: {}
-    //     },
-    //     series: [{
-    //         type: 'scatter',
-    //         // 使用百度地图坐标系
-    //         coordinateSystem: 'bmap',
-    //         // 数据格式跟在 geo 坐标系上一样，每一项都是 [经度，纬度，数值大小，其它维度...]
-    //         data: [ [120, 30, 1] ]
-    //     }]
-    // }
-    // // 获取百度地图实例，使用百度地图自带的控件
-    // console.log(echarts);
-    // console.log(bmap);
-    
-    // var bmap = echarts.getModel().getComponent('bmap').getBMap()
-    // bmap.addControl(new BMap.MapTypeControl());
-  
-  //   var _this = this;
 
-  //   _this.echartsModel = echarts.init(areamap)
-  //   _this.echartsModel.setOption({
-  //       bmap: {
-  //           roam: true, // 用户是否可以拖放和缩放地图
-  //           opt: { 
-  //             enableMapClick: false, //禁止地图底图的点击事件
-  //           }
-  //       },
-  //       animation: false,//关闭动画效果，这样在地图上覆盖散点图或者热力图的时候，拖动将不会有比较长时间的延迟
-  //       series: [],
-  //   });
 
-  //   // 添加百度地图插件bmp，获得地图对象，获取到的百度地图对象适用于所有的百度地图的接口。
-  //   _this.mapModel = _this.echartsModel.getModel().getComponent('bmap').getBMap();
-  //   //设置地图的中心点和缩放级别
-  //   _this.mapModel.centerAndZoom(new BMap.Point(_this.centerLng,_this.centerLat),_this.zoom);
-    
-  }
+    },
+    methods:{
+    }
 
-  
+
 }
 </script>
-<style>
-  #arealdistribution {
-    width: 100%;
-    height: calc(100vh - 64px);
-    /* background-color: tomato; */
-  }
-  .area_map {
-    width: 100%;
-    height: 100%;
-  }
+<style  scoped>
+    @import url(//a.amap.com/jsapi_demos/static/demo-center/css/demo-center.css);
+    #container{
+        padding:0px;
+        margin: 0px;
+        width: 100%;
+        height: 100%;
+        position:absolute;
+    }
+
+    .info {
+      margin-top: 60px;
+      font-size: 16px;
+    }
+
+    .info span {
+      color: #0288d1;
+      font-size: 16px;
+    }
+
 </style>
